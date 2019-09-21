ae6f6ce5e8ef2f00f3cd2bcc047e6f179ae65fa4
bestsuwijak <42570047+bestsuwijak@users.noreply.github.com>
2019-09-21 15:05:53 +0700

    new project

--- app/src/main/java/buu/informatics/s59160141/parkingplace/CarData.kt
+++ app/src/main/java/buu/informatics/s59160141/parkingplace/CarData.kt
@@ -1,3 +1,6 @@
 package buu.informatics.s59160141.parkingplace
 
-data class CarData(var licensePlate: String = "",var carBrand: String = "",var name: String = "")
+data class CarData(var id: Int
+                   ,var licensePlate: String = ""
+                   ,var carBrand: String = ""
+                   ,var name: String = "")
--- app/src/main/java/buu/informatics/s59160141/parkingplace/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160141/parkingplace/MainActivity.kt
@@ -13,9 +13,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val slot1: CarData = CarData("","","")
-    private val slot2: CarData = CarData("","","")
-    private val slot3: CarData = CarData("","","")
+    private val slot1: CarData = CarData(0,"","","")
+    private val slot2: CarData = CarData(1,"","","")
+    private val slot3: CarData = CarData(2,"","","")
+    private val slots = arrayOf(slot1,slot2,slot3)
+    private var slotIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,15 +26,105 @@ class MainActivity : AppCompatActivity() {
         hide()
 
         binding.apply {
-            slot_1.setOnClickListener{
-                click1()
+            this.slots = this@MainActivity.slots
+            slot_1.setOnClickListener{view ->
+                click(view)
             }
-            slot_2.setOnClickListener{
-                click2()
+            slot_2.setOnClickListener{view ->
+                click(view)
             }
-            slot_3.setOnClickListener{
-                click3()
+            slot_3.setOnClickListener{view ->
+                click(view)
             }
+        }
+    }
+
+    private fun getSlot(view:View):Int {
+        when(view.id) {
+            R.id.slot_1 -> return 0
+            R.id.slot_2 -> return 1
+            R.id.slot_3 -> return 2
+        }
+        return -1
+    }
+
+    private fun setButtonColor(int: Int){
+        when(int){
+            0 -> {slot_1.setBackgroundResource(R.color.slotSelect)
+                slot_2.setBackgroundResource(R.color.slot)
+                slot_3.setBackgroundResource(R.color.slot)
+            }
+            1 -> {slot_2.setBackgroundResource(R.color.slotSelect)
+                slot_1.setBackgroundResource(R.color.slot)
+                slot_3.setBackgroundResource(R.color.slot)
+            }
+            2 -> {slot_3.setBackgroundResource(R.color.slotSelect)
+                slot_1.setBackgroundResource(R.color.slot)
+                slot_2.setBackgroundResource(R.color.slot)
+            }
+        }
+    }
+
+    private fun click(view: View) {
+        show()
+        setButtonColor(getSlot(view))
+        binding.apply {
+            slotIndex = getSlot(view)
+            this.carData = slots?.get(slotIndex)
+            invalidateAll()
+
+            update_button.setOnClickListener{view ->
+                update(slotIndex)
+            }
+            delete_button.setOnClickListener{view ->
+                delete(slotIndex)
+            }
+        }
+    }
+
+    private fun update(int: Int){
+        when(int) {
+            0 -> { if(slot_1.text != ""){
+                    slot_1.text = licensePlate.text.toString()
+                }else{
+                    slot_1.text = "Empty"
+                }
+            }
+            1 -> { if(slot_2.text != ""){
+                    slot_2.text = licensePlate.text.toString()
+                }else{
+                    slot_2.text = "Empty"
+                }
+            }
+            2 -> { if(slot_3.text != ""){
+                    slot_3.text = licensePlate.text.toString()
+                }else{
+                    slot_3.text = "Empty"
+                }
+            }
+        }
+    }
+
+    private fun delete(int: Int){
+        binding.apply {
+            when(int) {
+                0 -> {slot_1.text = "Empty"
+                    licensePlate.text.clear()
+                    carBrand.text.clear()
+                    name.text.clear()
+                }
+                1 -> {slot_2.text = "Empty"
+                    licensePlate.text.clear()
+                    carBrand.text.clear()
+                    name.text.clear()
+                }
+                2 -> {slot_3.text = "Empty"
+                    licensePlate.text.clear()
+                    carBrand.text.clear()
+                    name.text.clear()
+                }
+            }
+            invalidateAll()
         }
     }
 
@@ -53,110 +145,6 @@ class MainActivity : AppCompatActivity() {
             name.visibility = View.VISIBLE
             update_button.visibility = View.VISIBLE
             delete_button.visibility = View.VISIBLE
-        }
-    }
-
-    private fun clickSlot(){
-//        binding.apply {
-//            slot_1.setOnClickListener{
-//                click1()
-//            }
-//            slot_2.setOnClickListener{
-//
-//            }
-//            slot_3.setOnClickListener{
-//
-//            }
-//        }
-    }
-
-    private fun click1(){
-        show()
-
-        binding.licensePlate.setText(slot1.licensePlate)
-        binding.carBrand.setText(slot1.carBrand)
-        binding.name.setText(slot1.name)
-
-        update_button.setOnClickListener{
-            if(licensePlate.text.toString() != "" && carBrand.text.toString() != "" && name.text.toString() != ""){
-                slot1.licensePlate = licensePlate.text.toString()
-                slot1.carBrand = carBrand.text.toString()
-                slot1.name = name.text.toString()
-                slot_1.setBackgroundColor(Color.RED)
-                hide()
-
-            }
-        }
-
-        delete_button.setOnClickListener{
-            binding.licensePlate.setText("")
-            binding.carBrand.setText("")
-            binding.name.setText("")
-            slot1.licensePlate = licensePlate.text.toString()
-            slot1.carBrand = carBrand.text.toString()
-            slot1.name = name.text.toString()
-            slot_1.setBackgroundResource(R.color.slot)
-            hide()
-        }
-    }
-
-    private fun click2(){
-        show()
-
-        binding.licensePlate.setText(slot2.licensePlate)
-        binding.carBrand.setText(slot2.carBrand)
-        binding.name.setText(slot2.name)
-
-        update_button.setOnClickListener{
-            if(licensePlate.text.toString() != "" && carBrand.text.toString() != "" && name.text.toString() != ""){
-                slot2.licensePlate = licensePlate.text.toString()
-                slot2.carBrand = carBrand.text.toString()
-                slot2.name = name.text.toString()
-                slot_2.setBackgroundColor(Color.RED)
-                hide()
-
-            }
-        }
-
-        delete_button.setOnClickListener{
-            binding.licensePlate.setText("")
-            binding.carBrand.setText("")
-            binding.name.setText("")
-            slot2.licensePlate = licensePlate.text.toString()
-            slot2.carBrand = carBrand.text.toString()
-            slot2.name = name.text.toString()
-            slot_2.setBackgroundResource(R.color.slot)
-            hide()
-        }
-    }
-
-    private fun click3(){
-        show()
-
-        binding.licensePlate.setText(slot3.licensePlate)
-        binding.carBrand.setText(slot3.carBrand)
-        binding.name.setText(slot3.name)
-
-        update_button.setOnClickListener{
-            if(licensePlate.text.toString() != "" && carBrand.text.toString() != "" && name.text.toString() != ""){
-                slot3.licensePlate = licensePlate.text.toString()
-                slot3.carBrand = carBrand.text.toString()
-                slot3.name = name.text.toString()
-                slot_3.setBackgroundColor(Color.RED)
-                hide()
-
-            }
-        }
-
-        delete_button.setOnClickListener{
-            binding.licensePlate.setText("")
-            binding.carBrand.setText("")
-            binding.name.setText("")
-            slot3.licensePlate = licensePlate.text.toString()
-            slot3.carBrand = carBrand.text.toString()
-            slot3.name = name.text.toString()
-            slot_3.setBackgroundResource(R.color.slot)
-            hide()
         }
     }
 
